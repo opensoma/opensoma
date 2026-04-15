9918a81a184695952e34e6239dae748540563dc9
Jeon Suyeol <devxoul@gmail.com>
2026-04-15 19:12:25 +0900

    Add mentoring edit page to web app

---
+++ apps/web/src/app/(main)/mentoring/[id]/edit/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '멘토링 수정',
+}
+
+export default function MentoringEditLayout({ children }: { children: React.ReactNode }) {
+  return children
+}
